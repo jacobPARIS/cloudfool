84185c09e20b67f119a3e2b909afbf6c199cd39a
JacobParis <jacob@jacobpariseau.com>
2021-06-07 01:06:49 +0800

    Make page header white

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -1,10 +1,10 @@
 import Link from 'next/link'
 export default function PageHeader() {
   return (
-    <header className="flex justify-between bg-blue-950">
-      <div className="px-4 py-2 text-lg text-blue-100">
+    <header className="flex justify-between bg-white border-b">
+      <div className="px-4 pt-3 text-lg">
         <Link href="/">
-          <a className="text-blue-100 hover:text-blue-200">Jacob Paris</a>
+          <a className="text-blue-950 hover:text-blue-800">Jacob Paris</a>
         </Link>
       </div>
 
@@ -12,7 +12,7 @@ export default function PageHeader() {
         <ul className="flex">
           <li className="px-4 py-2 text-lg text-blue-100">
             <Link href="/blog">
-              <a className="text-blue-100 hover:text-blue-200">Articles</a>
+              <a className="text-blue-950 hover:text-blue-800">Articles</a>
             </Link>
           </li>
         </ul>
